ace9f5864258a8c429aa11ae215283c5e6891b54
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-08-15 10:35:49 +0530

    Implement search feature

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,16 +1,22 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 
 export const ShopContext = createContext();
 
-const currency = "$";
-const deliveryFee = 10;
-
 const ShopContextProvider = (props) => {
+  const currency = "$";
+  const deliveryFee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+
   const value = {
     products,
     currency,
     deliveryFee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
   };
 
   return <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>;
