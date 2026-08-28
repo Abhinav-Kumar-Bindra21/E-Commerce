8e7ef1c8d78b8434865f1b7907a40d4705597439
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-08-28 09:59:59 +0530

    fix typo

--- backend/src/middleware/adminAuth.middleware.ts
+++ backend/src/middleware/adminAuth.middleware.ts
@@ -4,16 +4,12 @@ import { Request, Response, NextFunction } from "express";
 export const adminAuth = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { token } = req.headers;
-    console.log(token);
 
     if (!token) {
       return res.status(401).json({ success: false, message: "Not Authorized Login Again!!" });
     }
 
     const decode = jwt.verify(token, process.env.JWT_SECRET as string);
-
-    console.log("decode:", decode.email);
-    console.log("expected:", process.env.ADMIN_EMAIL);
 
     if (decode.email !== process.env.ADMIN_EMAIL) {
       return res.status(401).json({ success: false, message: "Not Authorized Login Again!!" });
