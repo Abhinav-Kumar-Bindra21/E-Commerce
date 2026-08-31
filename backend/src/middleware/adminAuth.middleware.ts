6d9213dd42ca54d9d77d78b2ef1409ca93da0a7f
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-08-31 09:33:30 +0530

    implement add product feature

--- backend/src/middleware/adminAuth.middleware.ts
+++ backend/src/middleware/adminAuth.middleware.ts
@@ -4,14 +4,12 @@ import { Request, Response, NextFunction } from "express";
 export const adminAuth = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { token } = req.headers;
-    console.log(token);
 
     if (!token) {
       return res.status(401).json({ success: false, message: "Not Authorized Login Again!!" });
     }
 
     const decode = jwt.verify(token, process.env.JWT_SECRET as string);
-    console.log(decode);
 
     if (decode.emailId !== process.env.ADMIN_EMAIL) {
       return res.status(401).json({ success: false, message: "Not Authorized Login Again!!" });
